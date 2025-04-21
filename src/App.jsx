da400efe0af19ab76a1c71cd32a7ea53ace1a707
tanvir <tanvirulislam149@gmail.com>
2025-04-21 11:09:55 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-8xl">This is phimart site</h1>
+      <p>Just checking</p>
+    </>
+  );
+}
+
+export default App;
---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Route, Routes } from "react-router";
+import MainLayout from "../layouts/MainLayout";
+import Home from "../pages/Home/Home";
+import Products from "../pages/Products/Products";
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="products" element={<Products />} />
+        {/* <Route path="about" element={<About />} /> */}
+      </Route>
+    </Routes>
+  );
+};
+
+export default AppRoutes;
